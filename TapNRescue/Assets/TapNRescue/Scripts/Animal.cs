99299dd1b7e0c246c7a4120f41881e33db379ddd
bayramtore <32714083+bayramtore@users.noreply.github.com>
2024-07-23 15:39:21 -0400

    before adding bus jam

--- TapNRescue/Assets/TapNRescue/Scripts/Animal.cs
+++ TapNRescue/Assets/TapNRescue/Scripts/Animal.cs
@@ -10,7 +10,7 @@ public enum AnimalDirection
     Down
 }
 
-[RequireComponent(typeof(CharacterController), typeof(Animator))]
+//[RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class Animal : MonoBehaviour
 {
     [Header("VARIABLES")]
@@ -20,13 +20,16 @@ public class Animal : MonoBehaviour
 
     bool isTapped = false;
     bool readyToTap = true;
+    [SerializeField] float speed = 10;
+    [SerializeField] float waitTimeToReset = 0.15f;
+    [SerializeField] Vector3 returnPos;
     #endregion Variables;
 
-    [Space(5), Header("CHARACTERCONTROLLER")]
-    #region CHARACTERCONTROLLER
-    public CharacterController characterController;
-    public float speed = 10;
-    #endregion CharacterController
+    //[Space(5), Header("CHARACTERCONTROLLER")]
+    //#region CHARACTERCONTROLLER
+    //public CharacterController characterController;
+    //public float speed = 10;
+    //#endregion CharacterController
 
     [Space(5), Header("ANIMATIONS")]
     #region ANIMATIONS
@@ -57,7 +60,7 @@ public class Animal : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        characterController = GetComponent<CharacterController>();
+        //characterController = GetComponent<CharacterController>();
         animator.SetFloat("IdleSpeed", Random.Range(0.75f, 1.25f));
         animator.SetFloat("WalkSpeed", walkSpeed);
         switch (animalDirection)
@@ -82,7 +85,8 @@ public class Animal : MonoBehaviour
     {
         if (isTapped)
         {
-            characterController.Move(speed * Time.deltaTime * transform.forward);
+            //characterController.Move(speed * Time.deltaTime * transform.forward);
+            transform.position += speed * Time.deltaTime * transform.forward;
         }
     }
 
@@ -105,6 +109,33 @@ public class Animal : MonoBehaviour
             isTapped = false;
             animator.SetBool(animatorHashes[(int)AnimationClips.Walk], false);
             animator.SetBool(animatorHashes[(int)AnimationClips.Hit], true);
+
+            switch (animalDirection)
+            {
+                case AnimalDirection.Up:
+                    returnPos = new(transform.position.x, transform.position.y, other.transform.position.z - 1);
+                    LeanTween.moveZ(this.gameObject, transform.position.z - 0.5f, 0.1f);
+                    break;
+                case AnimalDirection.Down:
+                    returnPos = new(transform.position.x, transform.position.y, other.transform.position.z + 1);
+                    LeanTween.moveZ(this.gameObject, transform.position.z + 0.5f, 0.1f);
+                    break;
+                case AnimalDirection.Right:
+                    returnPos = new(other.transform.position.x - 1, transform.position.y, transform.position.z);
+                    LeanTween.moveX(this.gameObject, transform.position.x - 0.5f, 0.1f);
+                    break;
+                case AnimalDirection.Left:
+                    returnPos = new(other.transform.position.x + 1, transform.position.y, transform.position.z);
+                    LeanTween.moveX(this.gameObject, transform.position.x + 0.5f, 0.1f);
+                    break;
+            }
+
+
+            //returnPos = new((int)transform.position.x, transform.position.y, (int)transform.position.z);
+            //Vector3 _returnPos = new((int)transform.position.x, transform.position.y, (int)transform.position.z);
+            //LeanTween.moveLocalZ(gameObject, transform.localPosition.z + 0.5f, 0.1f).setSpeed(5).setEaseInOutBack();
+            //Vector3 _returnPos = new((int)transform.position.x, transform.position.y, (int)transform.position.z);
+            //ResetAnimal(_returnPos);
         }
     }
 
@@ -119,14 +150,41 @@ public class Animal : MonoBehaviour
             return;
         isTapped = true;
         readyToTap = false;
+        animator.SetBool(animatorHashes[(int)AnimationClips.Idle], false);
         animator.SetBool(animatorHashes[(int)AnimationClips.Walk], true);
     }
 
-    
+    IEnumerator resetAnimalCoroutine;
+    IEnumerator ResetAnimalCoroutine(Vector3 returnPos)
+    {
+        yield return new WaitForSeconds(waitTimeToReset);
+        animator.SetBool(animatorHashes[(int)AnimationClips.Hit], false);
+        animator.SetBool(animatorHashes[(int)AnimationClips.Idle], true);
+        LeanTween.move(gameObject, returnPos, 0.5f).setSpeed(5).setOnComplete(() =>
+        {
+            readyToTap = true;
+        });
+    }
 
     public void ResetAnimal()
     {
+        //Vector3 _returnPos = new((int)transform.position.x, transform.position.y, (int)transform.position.z);
+        animator.SetBool(animatorHashes[(int)AnimationClips.Hit], false);
+        animator.SetBool(animatorHashes[(int)AnimationClips.Idle], true);
+        LeanTween.move(gameObject, returnPos, 0.5f).setSpeed(5).setEaseInBack().setOnComplete(() =>
+        {
+            readyToTap = true;
+        });
+    }
 
+    public void ResetAnimal(Vector3 returnPos)
+    {
+        if(resetAnimalCoroutine != null)
+        {
+            StopCoroutine(resetAnimalCoroutine);
+        }
+        resetAnimalCoroutine = ResetAnimalCoroutine(returnPos);
+        StartCoroutine(resetAnimalCoroutine);
     }
 
 
